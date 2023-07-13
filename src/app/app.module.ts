6a4c4b5a7b6ac8a9cae8de65447e09876966e0c1
Jebison <jebison.in@outlook.com>
2023-07-13 23:39:29 +0530

    UI changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,18 +14,21 @@ import { FieldsetModule } from 'primeng/fieldset';
 import { DropdownModule } from "primeng/dropdown";
 import { ToolbarModule } from 'primeng/toolbar';
 import { FormsModule } from '@angular/forms';
+import { CardModule } from 'primeng/card';
 
 import { BrowserAnimationsModule } 
     from "@angular/platform-browser/animations";
 import RegisterService from './Models/register.service';
 import { HttpClientModule } from '@angular/common/http';
+import { AdminComponent } from './admin/admin.component';
 
     
 export const appRoutes: Routes = [  
 { path: '', component: DashboardComponent },  
 { path: 'Register',component: RegisterComponent},  
 { path: 'login',component: LoginComponent},
-{ path: 'AddListing',component: AddListingComponent}];   
+{ path: 'AddListing',component: AddListingComponent},
+{ path: 'Admin', component: AdminComponent}];   
 @NgModule({
   declarations: [
     AppComponent,
@@ -33,6 +36,7 @@ export const appRoutes: Routes = [
     LoginComponent,
     RegisterComponent,
     AddListingComponent,
+    AdminComponent,
     
   ],
   imports: [
@@ -49,6 +53,7 @@ export const appRoutes: Routes = [
     ToolbarModule,
     FieldsetModule,
     ButtonModule,
+    CardModule,
     RouterModule.forRoot(appRoutes) 
   ],
   providers: [RegisterService],
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,12 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppModule } from './app/app.module';
-
+import { AdminComponent } from 'src/app/admin/admin.component';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
+
+  export const appRoutes: Routes = [
+    { path: 'Admin', component: AdminComponent}
+  ];
